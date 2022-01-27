6d79f175050d81bf0bb27b5edcb1aa118059fcda
Jeff Bush <jeffbushdesign@gmail.com>
2022-01-27 12:46:02 -0500

    Assert Equals

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🟢 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🔴 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Jeff", "Jeff");
+assertEqual(1, 17);
