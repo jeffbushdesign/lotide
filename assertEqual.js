734f56376d68e71fd8be70634dca34dae83c4f87
Jeff Bush <jeffbushdesign@gmail.com>
2022-01-28 12:57:36 -0500

    Updated assertEqual to be using template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,14 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
+    // Not using template literals
     console.log("🟢 Assertion Passed: " + actual + " === " + expected);
+    // Using template literals
+    console.log(`🟢 Assertion Passed: ${actual} === expected`);
   } else {
+    // Not using template literals
     console.log("🔴 Assertion Failed: " + actual + " !== " + expected);
+    // Using template literals
+    console.log(`🔴 Assertion Failed: ${actual} !== expected`);
   }
 };
 
