eb73f118458bfe81770f7adbe234165e1c37f7b7
Jeff Bush <jeffbushdesign@gmail.com>
2022-02-04 15:36:23 -0500

    Added countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const countLetters = (string) => {
+  const result = {}; // object to hold final result object
+  // const array = string.split('');
+  for (const char of string) {
+    // console.log(char);
+    if (char === ' ' || char === '.') {
+      continue;
+    } else if (!result[char]) {
+      result[char] = 1;
+      // console.log('peanuts');
+    } else {
+      result[char]++;
+    }
+    
+  }
+  console.log(result);
+  // return result;
+};
+
+
+countLetters('I went to school today.');
+
+
