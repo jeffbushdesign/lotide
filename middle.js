9522460461ba3502c477c6d5d3814cc730972f64
Jeff Bush <jeffbushdesign@gmail.com>
2022-02-07 15:46:27 -0500

    Added use case if empty array

--- middle.js
+++ middle.js
@@ -32,6 +32,9 @@ const assertArraysEqual = (array1, array2) => {
 
 // ACTUAL FUNCTION
 const middle = function(array) {
+  if (!array.length) // if array is empty, return undefined
+    return;
+
   if (array.length <= 2)
     return [];
   const mid = array.length / 2;
